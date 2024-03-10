7c8c9565b6362f3378e6de6268fd76970f9f12ed
Bryson Lindy <bslindy@gmail.com>
2024-03-09 16:08:50 -0800

    added index2 for transition to self-made html/css. added scripts make collapseable hamburger menu and resize function to preserve qualities between media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,32 @@
 function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log("logging out user");
-      }).catch((error) => {
-        // An error happened.
-      });
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    console.log("logging out user");
+  }).catch((error) => {
+    // An error happened.
+  });
 }
 
-function sayHello() {
-    
-}
-//sayHello();
+document.getElementById('mobile-menu').addEventListener('click', function () {
+  // Checks to make sure it's mobile query to prevent breaking the navbar in desktop mode
+  if (window.innerWidth <= 700) {
+    var menu = document.querySelector('.navbar-menu');
+    if (menu.style.display === "flex") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+      menu.style.flexDirection = "column";
+    }
+  }
+});
+
+window.addEventListener('resize', function () {
+  var menu = document.querySelector('.navbar-menu');
+  // If media query returns to desktop mode, fixed broken navbar
+  if (window.innerWidth > 700) {
+    menu.style.display = "";
+    menu.style.flexDirection = "row";
+  } else {
+    menu.style.flexDirection = "column";
+  }
+});
